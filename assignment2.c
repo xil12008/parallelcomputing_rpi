3ad40825f5a8c977dd0ca3068f38413b61c3c4a4
xil12008 <luxy622@gmail.com>
2016-02-17 16:51:50 -0500

    final

--- assignment2.c
+++ assignment2.c
@@ -29,14 +29,14 @@
 /***************************************************************************/
 
 // Bring over from your code
-#define NROWS 128
+#define NROWS 1024 
 
 unsigned int **g_GOL_CELL=NULL;
 unsigned int **g_GOL_CELL_TMP=NULL;
 unsigned int g_x_cell_size=0;
 unsigned int g_y_cell_size=0;
-unsigned int g_num_ticks=0;
-double THRESHOLD;
+unsigned int g_num_ticks=100;
+double THRESHOLD = 0.25;
 
 int mpi_myrank;
 int mpi_commsize;
@@ -50,7 +50,7 @@ int mpi_commsize;
 
 void allocate_and_init_cells();
 void compute_one_tick();
-void output_final_cell_state();
+void output_final_cell_state(int t);
 
 
 /***************************************************************************/
@@ -59,7 +59,7 @@ void output_final_cell_state();
 
 int main(int argc, char *argv[])
 {
-    double starttime, endtime;
+    double starttime = 0, endtime = 0;
     int top, bottom;
     unsigned int rbuffer_top[NROWS], rbuffer_bottom[NROWS];
     unsigned int sbuffer_top[NROWS], sbuffer_bottom[NROWS];
@@ -90,18 +90,21 @@ int main(int argc, char *argv[])
 
     g_y_cell_size = NROWS / mpi_commsize + 2; // 2 extra ghost rows  
     g_x_cell_size = NROWS; 
-    g_num_ticks = 100; 
-    THRESHOLD = 0;
 
     allocate_and_init_cells();
 
     for ( i = 0;i < g_num_ticks; ++i)
     {
 //Communication
-      memcpy(sbuffer_top,  (char *)g_GOL_CELL + 1 * g_x_cell_size * sizeof(unsigned int), g_x_cell_size * sizeof(unsigned int)); 
-      memcpy(sbuffer_bottom,  (char *)g_GOL_CELL + (g_y_cell_size - 2) * g_x_cell_size * sizeof(unsigned int), g_x_cell_size * sizeof(unsigned int)); 
-      sbuffer_top[0] = mpi_myrank;
-      sbuffer_bottom[0] = mpi_myrank;
+
+      //memcpy(sbuffer_top,  (char *)g_GOL_CELL + 1 * g_x_cell_size * sizeof(unsigned int), g_x_cell_size * sizeof(unsigned int)); 
+      //memcpy(sbuffer_bottom,  (char *)g_GOL_CELL + (g_y_cell_size - 2) * g_x_cell_size * sizeof(unsigned int), g_x_cell_size * sizeof(unsigned int)); 
+
+      for (j = 0;j < g_x_cell_size; ++j)
+      {
+        sbuffer_top[j] = g_GOL_CELL[j][1];
+        sbuffer_bottom[j] = g_GOL_CELL[j][g_y_cell_size - 2];
+      }
 
       MPI_Irecv(rbuffer_top, NROWS , MPI_UNSIGNED, top, 123, MPI_COMM_WORLD, &rrequest_top);
       MPI_Irecv(rbuffer_bottom, NROWS , MPI_UNSIGNED, bottom, 123, MPI_COMM_WORLD, &rrequest_bottom);
@@ -111,21 +114,23 @@ int main(int argc, char *argv[])
       MPI_Wait(&rrequest_bottom, &status);
       MPI_Wait(&srequest_top, &status);
       MPI_Wait(&srequest_bottom, &status);
-      printf("Rank %d of %d received top%d,bottom%d in tick %d\n", 
-               mpi_myrank, mpi_commsize, rbuffer_top[0], rbuffer_bottom[0], i);
+      //printf("Rank %d of %d received top%d,bottom%d in tick %d\n", 
+      //         mpi_myrank, mpi_commsize, rbuffer_top[0], rbuffer_bottom[0], i);
 
       for (j = 0;j < g_x_cell_size; ++j)
       {
-        g_GOL_CELL[0][i] = rbuffer_top[i];
-        g_GOL_CELL[g_y_cell_size - 1][i] = rbuffer_bottom[i];
+        g_GOL_CELL[j][0] = rbuffer_top[j];
+        g_GOL_CELL[j][g_y_cell_size - 1] = rbuffer_bottom[j];
       }
+
       //memcpy(g_GOL_CELL, rbuffer_top, g_x_cell_size * sizeof(unsigned int)); 
       //memcpy((char *)g_GOL_CELL + (g_y_cell_size - 1) * g_x_cell_size * sizeof(unsigned int), (char *)rbuffer_bottom, g_x_cell_size * sizeof(unsigned int)); 
+
 //Game of Life Simulation(serial)
-      
       compute_one_tick();
     }
-    output_final_cell_state();
+
+    //output_final_cell_state(i);
 
 // END -Perform a barrier and then leave MPI
     MPI_Barrier( MPI_COMM_WORLD );
@@ -217,6 +222,7 @@ void compute_one_tick()
     for ( j = 0;j < g_y_cell_size;++j)
     {
           g_GOL_CELL[i][j] = g_GOL_CELL_TMP[i][j]; 
+
     }
   }
 }
@@ -226,26 +232,26 @@ void compute_one_tick()
 /* Function: output_final_cell_state ***************************************/
 /***************************************************************************/
 
-void output_final_cell_state()
+void output_final_cell_state(int t)
 {
   char filename[256];
-  sprintf(filename, "cellgraph_rank%d_%d.txt", 
-                          mpi_myrank, mpi_commsize);
+  sprintf(filename, "%0.2fthersdhold_rank%dof%d_size%d*%d.txt", 
+                          THRESHOLD, mpi_myrank, mpi_commsize, g_x_cell_size, NROWS);
   FILE * fp = fopen(filename, "w+");
   if (fp == NULL) perror("FILE OPEN FAILED");
   
   // print out in grid form 16 value per row of the g_GOL_CELL
   // This data will be used to create your graphs
   int i = 0, j=0;
-  fprintf(fp, "[");
-  for (i = 0;i < g_y_cell_size;++i)
+  //fprintf(fp, "[");
+  for (i = 1; i < g_y_cell_size -1 ;++i)
   {
     for ( j = 0;j < g_x_cell_size - 1;++j)
     {
-      fprintf(fp, "%d ", g_GOL_CELL[i][j]);
+      fprintf(fp, "%u ", g_GOL_CELL[j][i]);
     }
-    fprintf(fp, "%d;\n ", g_GOL_CELL[i][g_x_cell_size - 1]);
+    fprintf(fp, "%u;\\\n ", g_GOL_CELL[j][g_x_cell_size - 1]);
   }
-  fprintf(fp, "]%d*%d\n", g_x_cell_size, g_y_cell_size);
+  //fprintf(fp, "]%d*%d\n", g_x_cell_size, g_y_cell_size);
   fclose(fp);
 }
